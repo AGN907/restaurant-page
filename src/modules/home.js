94af531540de9b5f748ef82f998e4932da86a6c6
AGN <itzme907@gmail.com>
2023-08-10 18:03:15 +0300

    Add home module

---
+++ src/modules/home.js
@@ -0,0 +1,29 @@
+export default function Home() {
+  const layout = ` <h1>Burger Ming Restaurant</h1>
+  <div class="content-grid">
+  <div>
+      <h3>About</h3>
+      <p>
+          Burger Ming has the best burgers in the universe! With our perfect
+          ingredients and our amazing meat, you will get so addicted that you
+          will visit us at least twice a day!
+        </p>
+  </div>
+  <div>
+      <h3>Hours</h3>
+      <p>Sunday: 8am - 8pm</p>
+      <p>Monday: 6am - 6pm</p>
+      <p>Tuesday: 6am - 6pm</p>
+      <p>Wednesday: 6am - 6pm</p>
+      <p>Thursday: 6am - 10pm</p>
+      <p>Friday: 6am - 10pm</p>
+      <p>Saturday: 8am - 10pm</p>
+  </div>
+  <div>
+      <h3>Location</h3>
+      <p>123 Forest Driver, Forestville, Maine</p>
+  </div>
+  </div>`;
+
+  return layout
+}
